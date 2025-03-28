4c0946419ec8db342ba866f4c8aef6a41c760d39
CoreDiLeone <fireejuegos@gmail.com>
2025-03-28 14:13:19 -0300

    Fixed the configuration between GitHub Pages and the React DOM Router.

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./components/Home";
 import Product from "./components/Product";
 
-const basename = '/simple_coffee_listing/'; 
+
 function App() {
   
   return (
-    <BrowserRouter basename={basename}>
+    <BrowserRouter>
     <Routes>
       <Route path='/' element={ <Home />}/>
       <Route path='/product' element={ <Product />}/>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+import Home from '../src/components/Home.jsx';
+import Product from '../src/components/Product.jsx';
 
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/product',
+    element: <Product />
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <RouterProvider router= {router}>
     <App />
+    </RouterProvider>
   </StrictMode>,
 )
